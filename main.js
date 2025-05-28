3aab9c48f3b7062952d0826a15be2909d2878259
moniktamburi <monik.araujo@escola.pr.gov.br>
2025-05-28 20:58:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = decument.querySelectorAll(".aba")
+const textos = decument.querySelectorAll(".aba-conteudo");
+
+for(let i = 0; i < botoes.length; j++)
